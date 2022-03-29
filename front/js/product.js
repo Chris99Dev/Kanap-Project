98f2ea862a4da3fb2c973a049ad8e075fe05ab78
ChrisF <ferrchristopher@gmail.com>
2022-03-29 11:41:21 +0200

    Product Page
    - Resolve error to show product information
    - Sarting adding to cart function

--- front/js/product.js
+++ front/js/product.js
@@ -1,14 +1,14 @@
 var loc = window.location.href;
 var url = new URL(loc);
 var idProduct = url.searchParams.get("id");
-let product = "";
+console.log(idProduct); //Show Id of the product
+var product = "";
 //Color choice
 var colorChoice = document.querySelector('#colors');
 //Quantity choice
 var productQuantity = document.querySelector('#quantity');
 
-// console.log(idProduct);
-// console.log(url);
+getProduct();
 
 //Recuperation des donnees de API
 function getProduct() {
@@ -32,27 +32,24 @@ function getProduct() {
 //Function Creation de la carte article
 function getPost(product) {
     //Insert Image
-    let productImg = document.createElement("img");
+    var productImg = document.createElement("img");
     document.querySelector(".item__img").appendChild(productImg);
     productImg.src = product.imageUrl;
-    productImg.alt = article.altTxt;
+    productImg.alt = product.altTxt;
 
     //Change H1
-    let productName = document.getElementById('title');
-    productName.innerHTML = product.name;
+    var productName = document.getElementById("title").innerHTML = product.name;
 
     //Change Price
-    let productPrice = document.getElementById('price');
-    productPrice.innerHTML = product.price;
+    var productPrice = document.getElementById('price').innerHTML = product.price;
 
     //Change Descritpion
-    let productDescription = document.getElementById('description');
-    productDescription.innerHTML = product.description;
+    var productDescription = document.getElementById('description').innerHTML = product.description;
 
     //Color List
-    for (let colors of product.colors) {
+    for (var colors of product.colors) {
         console.table(colors);
-        let productColors = document.createElement("option");
+        var productColors = document.createElement("option");
         document.querySelector("#colors").appendChild(productColors);
         productColors.value = colors;
         productColors.innerHTML = colors;
@@ -61,18 +58,30 @@ function getPost(product) {
     addToCart(product);
 }
 
-//Function ajout dans le panier
-function addToCart(product) {
-    const btn_addToCart = document.querySelector('#addToCart');
+// //Function ajout dans le panier
+// function addToCart(product) {
+//     const btn_addToCart = document.querySelector('#addToCart');
 
-    //Add Listener on Btn AddToCart
-    btn_addToCart.addEventListener("click", (event) => {
-        if (quantityPicked.value > 0 && quantityPicked.value <= 100 && quantityPicked.value != 0) {
+//     //Add Listener on Btn AddToCart
+//     btn_addToCart.addEventListener("click", (event) => {
+//         if (quantityPicked.value > 0 && quantityPicked.value <= 100 && quantityPicked.value != 0) {
 
-            //
-        }
-    })
-}
+//             let colorChoice = colorPicked.value;
+//             let productQuantity = quantityPicked.value;
+
+//             let infoProduct = {
+//                 idProduit: idProduct,
+//                 couleurProduit: colorChoice,
+//                 quantiteProduit: Number(productQuantity),
+//                 nomProduit: article.name,
+//                 prixProduit: article.price,
+//                 imgProduit: article.imageUrl
+//             };
+
+
+//         }
+//     })
+// }
 //recuperation du choix de couleur
 
 //Recuperation du choix du nombre entre 1 et 100
